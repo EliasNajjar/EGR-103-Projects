a383a44754c8557b733bdcdebde9cfd2bca470ac
EliasNajjar <eliasnajjar111@gmail.com>
2025-04-16 21:13:50 -0400

    game updates

--- Final_Board_Game.m
+++ Final_Board_Game.m
@@ -97,6 +97,11 @@ turnNum=0;
 
 
 previous_roll = 0;
+roi4dice = [141   154   216   141];
+croppingForRealign = [0 0 200 480];
+cam = webcam(2); % USB cam is the second
+s = serialport('COM4',9600);
+%preview(cam); % shows video
 
 while turnNum<= 10
     % Check whose turn it is
@@ -123,9 +128,6 @@ hold off
 % now let me roll a dice and get a random value, integer between 1 and 6
 MoveComplete=0;
 
-cam = webcam(2); % USB cam is the second
-%preview(cam); % shows video
-
 if mod(turnNum,2) == 0
     roll = previous_roll;
     dice_removed = false;
@@ -136,9 +138,6 @@ if mod(turnNum,2) == 0
         
         % this will make your picture appear on the screen.
         %imshow(image)
-        
-        roi4dice = [141   154   216   141];
-        %disp(roi4dice)
         
         croppedImage=imcrop(image,roi4dice);
         %imshow(croppedImage);
@@ -168,14 +167,115 @@ if mod(turnNum,2) == 0
             %disp("dice removed");
         end
     end
+    if roll < 1 || roll > 6
+        roll = randi(6);
+    end
     previous_roll = roll;
     dice = roll;
 else
     dice=randi(6);
 end
 
+% realign
+while true
+    image = snapshot(cam);
+    croppedImage=imcrop(image,croppingForRealign);
+    %imshow(croppedImage);
+    
+    % First I will get the values for each separated out:
+    r_channel=croppedImage(:,:,1);
+    g_channel=croppedImage(:,:,2);
+    b_channel=croppedImage(:,:,3);
+    
+    % I create variables for the ratios
+    rg_ratio=double(r_channel)./double(g_channel);% red green ratio
+    rb_ratio=double(r_channel)./double(b_channel);% red blue ratio
+    gb_ratio=double(g_channel)./double(b_channel);% green blue ratio
+    
+    rg_ratio(isnan(rg_ratio))=0;% if it is nan it sets it to zero
+    rb_ratio(isnan(rb_ratio))=0;% this should only happen if it is black
+    gb_ratio(isnan(gb_ratio))=0;
+    
+    %imtool(croppedImage)
+    
+    found = rg_ratio < .85 & gb_ratio > 1.2;
+    ImprovedPic=bwareaopen(found,1000); % gets rid of object smaller than 1000 pixels area
+    %imshow(ImprovedPic)
+    
+    filledHoles=imfill(found,'holes');
+    %imshow(filledHoles)
+    
+    blue_overlay = imoverlay(croppedImage,filledHoles,[0,0,1]);
+    %imshow(blue_overlay)
+    
+    centroids = regionprops('table',filledHoles,'Centroid');
+    centroids = centroids{:,:};
+    
+    %{
+    figure, imshow(croppedImage), title('Centroids for Alignment Stickers')
+    hold on
+        plot(centroids(:,1),centroids(:,2),'r+','MarkerSize',10,'LineWidth',2);
+    hold off
+    %}
+    
+    centroidslen = size(centroids);
+    index = 1;
+    for i = 1:centroidslen(1)
+        edge = false;
+        for x = -1:1
+            for y = -1:1
+                if ImprovedPic(round(centroids(index,2)+y),round(centroids(index,1)+x)) == false
+                    edge = true;
+                end
+            end
+        end
+        if edge
+            centroids(index,:) = [];
+            index = index - 1;
+        end
+        index = index + 1;
+    end
+    centroidslen = size(centroids);
 
-
+    if centroidslen(1) == 1
+        steps_for_1 = 1000;
+        % Append combines the various strings into one individual string to be sent over to the Arduino
+        Multiple_Stepper_String = append("1,",int2str(steps_for_1),",");
+        % Send the string to the Arduino using the connected serial port
+        write(s,Multiple_Stepper_String,'string');
+        % Pause to allow the previous movements to complete before sending new movements
+        pause(5)
+        
+    else
+        Centroid_Distance=sqrt(((centroids(1,1)-centroids(2,1))^2)+((centroids(1,2)-centroids(2,2))^2));
+        if Centroid_Distance < 110
+            break;
+        end
+        fprintf('The distance between centroids is %.2f pixels \n',Centroid_Distance);
+        if centroids(2,1) > centroids(1,1)
+            if centroids(2,2) < centroids(1,2)
+                steps_for_1 = 4 * Centroid_Distance - 400;
+            else
+                steps_for_1 = -4 * Centroid_Distance + 400;
+            end
+        else
+            if centroids(2,2) < centroids(1,2)
+                steps_for_1 = -4 * Centroid_Distance + 400;
+            else
+                steps_for_1 = 4 * Centroid_Distance - 400;
+            end
+        end
+        if abs(steps_for_1) < 50
+            steps_for_1 = steps_for_1 * 3;
+        end
+        % Append combines the various strings into one individual string to be sent over to the Arduino
+        Multiple_Stepper_String = append("1,",int2str(steps_for_1),",");
+        % Send the string to the Arduino using the connected serial port
+        write(s,Multiple_Stepper_String,'string');
+        % Pause to allow the previous movements to complete before sending new movements
+        pause(5)
+    end
+end
 
 %**********************************************************************
 %**********************************************************************
@@ -219,11 +319,11 @@ opportunities=0;
 for i= 1:14
     checkPosition=i;
     possibleMove=i+dice; % the piece will be that many spaces from where the piece is now
-    if BoardSetup(i,ColOfInterest)>=1;
+    if BoardSetup(i,ColOfInterest)>=1
         if possibleMove >=14 % Past the last position on the board
                 fprintf('Game piece in postion %d can move to home \n', checkPosition);
                 opportunities=opportunities+1;
-        elseif possibleMove<=13; % Not past the last position on the bard
+        elseif possibleMove<=13 % Not past the last position on the bard
             if BoardSetup(possibleMove,4)==0
                 fprintf('Game piece in position %d can move to position %d \n', checkPosition,possibleMove);
                 opportunities=opportunities+1;
